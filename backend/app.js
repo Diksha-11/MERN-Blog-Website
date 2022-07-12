fa0ab053bb8b7bbf9a0dea067df7c767c62b3ce0
Diksha Sharma <ns481183@gmail.com>
2022-07-12 16:21:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import blogRouter from "./routes/blog-router.js";
 import router from "./routes/user-routes";
-import cors from "cors";
+import cors from "cors";//Access-Control-Allow-Origin, 
+//allow all domain, This is done by bypassing the Access-Control-Allow-Origin headers, which specify which origins can access the API.
 
 
 const app = express()
@@ -10,7 +11,7 @@ const Port = process.env.PORT || 5000
 const {MONGOURI} = require('./keys')
 //require('/keys')
 
-app.use(cors());
+app.use(cors());//
 app.use(express.json());
 app.use("/api/user", router); //here giving url path e.g. http://localhost:5000/api/user/ ...all of routers will work
 app.use("/api/blog", blogRouter);
